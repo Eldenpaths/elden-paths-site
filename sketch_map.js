1b80b78b1a4dc1f7754364f30c6f5d68a8d3903c
Eldenpaths <patioandcamp@gmail.com>
2025-05-17 03:58:06 -0700

    Add files via upload

---
+++ sketch_map.js
@@ -0,0 +1,10 @@
+
+// Sketch map preview logic for Elden Paths
+const sketchArtByTerrain = {
+  "plains": "assets/sketches/plains.png",
+  "forest": "assets/sketches/forest.png",
+  "ruins": "assets/sketches/ruins.png",
+  "swamp": "assets/sketches/swamp.png",
+  "mountain": "assets/sketches/mountain.png",
+  "water": "assets/sketches/water.png"
+};
