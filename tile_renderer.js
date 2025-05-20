c5e03c1b23eb088179a5f3feb3619f60d70a0bfc
Eldenpaths <patioandcamp@gmail.com>
2025-05-19 22:04:47 -0700

    Update tile_renderer.js

--- tile_renderer.js
+++ tile_renderer.js
@@ -5,7 +5,7 @@ function loadTileMeta(callback) {
   fetch("tile_meta.json")
     .then((response) => response.json())
     .then((data) => {
-      window.tileMeta = data; // avoid declaring tileMeta twice
+      Object.assign(tileMeta, data); // DON'T re-declare tileMeta!
       callback();
     });
 }
@@ -15,17 +15,29 @@ function loadTileImages(callback) {
   let loaded = 0;
   const total = types.size;
 
-  types.forEach((filename) => {
+  types.forEach((type) => {
     const img = new Image();
-    img.src = `assets/img/tiles/${filename}`; // fixed: no extra `_tile.png`
+    img.src = `assets/img/tiles/${type}`;
     img.onload = () => {
-      tileImages[filename] = img;
+      tileImages[type] = img;
       loaded++;
       if (loaded === total) callback();
     };
     img.onerror = () => {
-      console.warn("Missing tile image for:", filename);
+      console.warn(`⚠️ Missing tile image for: ${type}`);
+      loaded++;
+      if (loaded === total) callback();
     };
   });
+}
+
+function drawTileMap() {
+  for (let y = 0; y < tileIndex.length; y++) {
+    for (let x = 0; x < tileIndex[y].length; x++) {
+      const type = tileIndex[y][x];
+      const img = tileImages[tileMeta[type]];
+      if (img) ctx.drawImage(img, x * tileSize, y * tileSize, tileSize, tileSize);
+    }
+  }
 }
 
