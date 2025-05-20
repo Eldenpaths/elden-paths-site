d913a1fed4409bbd1700af16a3cf309f5b2c4ada
Eldenpaths <patioandcamp@gmail.com>
2025-05-19 23:00:08 -0700

    Delete sketch_map.js

--- sketch_map.js
+++
@@ -1,6 +0,0 @@
-const sketchArtByTerrain = {
-  "plains": "assets/sketches/plains.png",
-  "forest": "assets/sketches/forest.png",
-  "mountain": "assets/sketches/mountain.png",
-  "ruins": "assets/sketches/sample.png"
-};
