c7d03c3b9f79e4b3b358ab3f63d96e8afa33393c
Eldenpaths <patioandcamp@gmail.com>
2025-05-27 04:50:51 -0700

    Delete scripts directory

--- scripts/admin_tools/bundle_cron_job_stub.sh
+++
@@ -1,7 +0,0 @@
-
-#!/bin/bash
-# Bundle Cron Job for Elden Parallel Build
-cd /mnt/data/elden_echo_compiler
-python3 world_bundler.py
-python3 run_bundler_server.py
-python3 bundle_export.py  # hypothetical if broken into stages
